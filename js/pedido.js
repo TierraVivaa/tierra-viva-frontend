7d07ee75dc0a8d2b11ba9c04796914a916aeb9f1
Maggievira87 <margarethjvr87@gmail.com>
2025-12-05 18:07:55 -0500

    Ultimo cambio conexio entre pedidos y productos

---
+++ js/pedido.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const listaPedidos = document.getElementById("listaPedidos");
+    const subtotalSpan = document.getElementById("subtotal");
+    const totalSpan = document.getElementById("total");
+
+    let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+    if (carrito.length === 0) {
+        listaPedidos.innerHTML = `
+            <div style="background:#fff; padding:20px; border-radius:10px; text-align:center;">
+                <h3>Tu carrito está vacío</h3>
+                <a href="/html/productos.html" style="color:#00b412; font-weight:bold;">Seguir comprando</a>
+            </div>
+        `;
+        subtotalSpan.textContent = "$0";
+        totalSpan.textContent = "$0";
+        return;
+    }
+
+    let subtotal = 0;
+
+    carrito.forEach(producto => {
+        const subtotalProd = producto.precio * producto.cantidad;
+        subtotal += subtotalProd;
+
+        listaPedidos.innerHTML += `
+            <div class="producto">
+                <img src="${producto.imagen}" alt="${producto.nombre}">
+                <div>
+                    <h3>${producto.nombre}</h3>
+                    <p>Unidad de venta: ${producto.unidad || "1 und"}</p>
+                    <p>Categoria: ${producto.categoria || "General"}</p>
+                    <p class="precio">$${producto.precio.toLocaleString()}</p>
+                    <p>Cantidad: ${producto.cantidad}</p>
+                </div>
+            </div>
+        `;
+    });
+
+    subtotalSpan.textContent = `$${subtotal.toLocaleString()}`;
+    totalSpan.textContent = `$${subtotal.toLocaleString()}`; 
+
+    
+    document.getElementById("btnConfirmarPedido").addEventListener("click", () => {
+        alert("¡Gracias por tu compra! 🛒🌱");
+        localStorage.removeItem("carrito");
+        window.location.href = "/html/productos.html";
+    });
+});
